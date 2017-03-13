377b87a70b751025a7c6570806c1121e3e8b0b64
EmilyH <EmilyHHLam@gmail.com>
2017-03-13 16:50:05 -0500

    fix the swap eerror

--- show.js
+++ show.js
@@ -2,33 +2,30 @@
 var currentIndex = 0;
 $(document).ready(function(){
 
-$('button').on("click", function() {
+  $('button').on("click", function() {
 
-current = currentIndex++;
+    current = currentIndex++;
 
-$('.container').append("<div class='smallContainer'></div>");
-var $el = $('.container').children().last();
-$el.append("<p></p><p>" + current + "</p>");
-$el.append("<button id= 'swap'>swap color</button>");
-$el.append("<button id='delete'>delete</button>");
-colorSwap();
-deleteContainer();
-});
+    $('.container').append("<div class='smallContainer'></div>");
+    var $el = $('.container').children().last();
+    $el.append("<p></p><p>" + current + "</p>");
+    $el.append("<button class='swap'>swap color</button>");
+    $el.append("<button class='delete'>delete</button>");
 
-function colorSwap() {
-  $('.smallContainer').on("click", "#swap", function(){
-    $(this).parent().addClass('switchColor');
-  //console.log("swap");
   });
-}
 
-function deleteContainer() {
-  $('.smallContainer').on("click", "#delete", function() {
+
+
+  $('.container').on("click", ".swap", function(){
+    //$(this).parent().addClass('switchColor');
+     $(this).parent().parent().toggleClass('switchColor');
+     console.log("swap");
+  });
+
+  $('.container').on("click", ".delete", function() {
     $(this).parent().remove();
     console.log('delete');
   });
 
 
-}//end of deleteContainer
-
-});//end of doc
+});//end of deleteContainer
